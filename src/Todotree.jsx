860b4c96983d0b1f383415a30f996074dc386e7d
Git-Raga <kraghavendran@gmail.com>
2025-04-01 05:15:07 +0530

    1st April from office

--- src/Todotree.jsx
+++ src/Todotree.jsx
@@ -9,9 +9,11 @@
  
 
 
-TODO // add the total tasks, closed, open , phover in while these and done tixk change
-TODO // Add the Done, Edit and Delete fucntionalities
-TODO // Done task animation
-TODO // Display all / active tasks dark theme background row color fix
+ 
+TODO // Delete fucntionalities
+TODO // Sorting logic
+TODO // Add the task comments section while creating task
+TODO // Logic to Done to Complete section
+TODO // sorting based on Due date
 TODO // Remove APAC and EMEA and have the new AMER team in the user details
 TODO // Side panel to be closed and opened
